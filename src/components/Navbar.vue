953f83d60a38da475d35cbfc57e6054d162a5951
maito <itomasa0915@gmail.com>
2020-05-05 14:15:26 +0900

    add new file

---
+++ src/components/Navbar.vue
@@ -0,0 +1,6 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-2">
+    <img alt="Vue logo" src="@/assets/logo.png" style="height: 50px">
+    <a class="navbar-brand" href="#">Vue Test</a>
+  </nav>
+</template>
