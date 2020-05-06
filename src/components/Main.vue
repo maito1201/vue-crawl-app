a5f3ddabcf637b44753085edb2567ba24dac2621
maito <itomasa0915@gmail.com>
2020-05-06 18:31:24 +0900

    modify grid design

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,7 +2,7 @@
   <div class='main'>
     <input @click="getItems" type="button" value="取得">
     <div class="row mr-2 ml-2">
-      <div v-for="item in items" :key="item.id" class="col-md-3 col-sm-6 p-2">
+      <div v-for="item in items" :key="item.id" class="col-md-3 col-sm-6 col-6 p-2">
         <a :href="item.Url" target="_blank">
           <div class="card p-0 pb-3 shadow">
             <img :src="item.Image" class="card-img-top">
