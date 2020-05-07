1e9a3370037cebbd3307eb866a934db2dbaab5a1
maito <itomasa0915@gmail.com>
2020-05-07 22:15:00 +0900

    multiple search

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,71 +1,107 @@
 <template>
-  <pull-to :top-load-method="getItems" :top-config="pullToConfig">
   <div class='main'>
-    <Loading :isLoading="isLoading"/>
-    <div class="row mr-2 ml-2">
-      <div class="input-group col-xl-3 col-md-6 p-2">
-        <input type="text" class="form-control" v-model="keyword">
-        <div class="input-group-append">
-          <button type="button" class="btn btn-outline-success" @click="getItems">Search</button>
-        </div>
-      </div>
-    </div>
-    <div class="col-12">
-      <div class="alert alert-secondary" v-if="items.length == 0">
-        <p class="m-0" >一致する商品はありません</p>
-        <p class="m-0" v-if="searched.length">キーワード: {{searched}}</p>
-      </div>
-    </div>
-    <div class="row mr-2 ml-2">
-      <div v-for="item in items" :key="item.id" class="col-md-3 col-sm-6 col-6 p-2">
-        <a :href="item.Url" target="_blank">
-          <div class="card p-0 pb-3 shadow">
-            <img :src="item.Image" class="card-img-top">
-            <p class="card-title">{{ item.Name }}</p>
-            <p class="card-text font-weight-bolder">{{ item.Price }}</p>
+    <carousel :per-page="1"  :mouse-drag="false">
+      <slide v-for="condition in conditions" :key="condition.id">
+        <Loading :isLoading="condition.isLoading"/>
+        <div class="row mr-2 ml-2">
+          <div class="input-group col-xl-3 col-md-6 p-2">
+            <input type="text" class="form-control" v-model="condition.keyword">
+            <div class="input-group-append">
+              <button type="button" class="btn btn-outline-success" @click="getItems(condition)">Search</button>
+            </div>
           </div>
-        </a>
-      </div>
-    </div>
+        </div>
+        <div class="col-12">
+          <div class="alert alert-secondary" v-if="condition.items.length == 0">
+            <p class="m-0" >一致する商品はありません</p>
+            <p class="m-0" v-if="condition.searched.length">キーワード: {{searched}}</p>
+          </div>
+        </div>
+        <div class="row mr-2 ml-2">
+          <div v-for="item in condition.items" :key="item.id" class="col-md-3 col-sm-6 col-6 p-2">
+            <a :href="item.Url" target="_blank">
+              <div class="card p-0 pb-3 shadow">
+                <img :src="item.Image" class="card-img-top">
+                <p class="card-title">{{ item.Name }}</p>
+                <p class="card-text font-weight-bolder">{{ item.Price }}</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </slide>
+    </carousel>
   </div>
-  </pull-to>
 </template>
 
 <script>
 import Loading from './Loading.vue'
-import PullTo from 'vue-pull-to'
+import { Carousel, Slide } from 'vue-carousel';
 export default {
   name: 'Main',
   components: {
     Loading,
-    PullTo
+    Carousel,
+    Slide
   },
   data: function (){
     return {
-      pullToConfig: {
-        pullText: 'pull to reload',
-        triggerText: 'release',
-        loadingText: '',
-        doneText: '',
-        failText: '',
-        loadedStayTime: 0,
-        stayDistance: 50,
-        triggerDistance: 70
-      },
-      items: [],
-      keyword: "",
-      searched: "",
-      searchPath: "/api/digimart?keyword=",
+      conditions: [
+        {
+          id: 0,
+          items: [],
+          keyword: "",
+          searched: "",
+          searchPath: "/api/digimart?keyword=",
+          isLoading: false,
+          cssClass: "carousel-item active"
+        },
+        {
+          id: 1,
+          items: [],
+          keyword: "",
+          searched: "",
+          searchPath: "/api/digimart?keyword=",
+          isLoading: false,
+          cssClass: "carousel-item"
+        },
+        {
+          id: 3,
+          items: [],
+          keyword: "",
+          searched: "",
+          searchPath: "/api/digimart?keyword=",
+          isLoading: false,
+          cssClass: "carousel-item"
+        },
+        {
+          id: 4,
+          items: [],
+          keyword: "",
+          searched: "",
+          searchPath: "/api/digimart?keyword=",
+          isLoading: false,
+          cssClass: "carousel-item"
+        },
+        {
+          id: 5,
+          items: [],
+          keyword: "",
+          searched: "",
+          searchPath: "/api/digimart?keyword=",
+          isLoading: false,
+          cssClass: "carousel-item"
+        }
+      ],
       isLoading: false
     }
   },
   methods: {
-    getItems(loaded){
+    getItems(condition, loaded){
       if (typeof(loaded) == "function") { loaded('done'); }
-      if(this.isLoading) { return }
-      this.isLoading = true;
+      if(condition.isLoading) { return }
+      condition.isLoading = true;
       this.axios.get(
-        `${this.searchPath}${this.keyword}`, {
+        `${condition.searchPath}${condition.keyword}`, {
           mode: 'no-cors',
           headers: {
             'Access-Control-Allow-Origin': '*',
@@ -75,13 +111,17 @@ export default {
           credentials: 'same-origin'
         }
       ).then((response) => {
-        this.searched = this.keyword;
-        this.items = response.data;
+        condition.searched = condition.keyword;
+        condition.items = response.data;
       }).catch((e) => {
         alert(e);
       }).finally(() => {
-        this.isLoading = false;
+        condition.isLoading = false;
       });
+    },
+    reloadItems(loaded){
+      const index = this.$refs.carousel ? this.$refs.carousel.currentPage : 0;
+      this.getItems(this.conditions[index], loaded);
     }
   }
 }
