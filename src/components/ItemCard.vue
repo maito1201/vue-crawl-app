b36b518c84f04e8b438089f3ec55cb051db5d588
maito <maito@appirits.com>
2020-05-09 13:17:19 +0900

    truncate title

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -2,17 +2,50 @@
   <a :href="item.Url" target="_blank">
     <div class="card p-0 pb-3 shadow">
       <img :src="item.Image" class="card-img-top">
-      <p class="card-title">{{ item.Name }}</p>
+      <p class="card-title">{{ item.Name | truncate }}</p>
       <p class="card-text font-weight-bolder">{{ item.Price }}</p>
     </div>
   </a>
 </template>
+
 <script>
+String.prototype.bytes = function () {
+  var length = 0;
+  for (var i = 0; i < this.length; i++) {
+    var c = this.charCodeAt(i);
+    if ((c >= 0x0 && c < 0x81) || (c === 0xf8f0) || (c >= 0xff61 && c < 0xffa0) || (c >= 0xf8f1 && c < 0xf8f4)) {
+      length += 1;
+    } else {
+      length += 2;
+    }
+  }
+  return length;
+};
+
 export default {
   name: 'ItemCard',
   props: {
     item: Object
   },
+  filters: {
+    truncate: function (str) {
+      const len = 20;
+      if (!str) return ''
+      let truncated = str.substr(0, len);
+      if (truncated.bytes() > 22) truncated = str.substr(0, 15)
+      return str.length <= len ? str : (truncated + "...");
+    }
+  }
 }
 </script>
+
+<style scoped>
+.card-title{
+  font-size: 12px;
+  color: black;
+}
+.card-text{
+  color: #B12704;
+}
+</style>
   
--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -3,6 +3,7 @@
     <vue-loading type="bubbles" color="#28a745" :size="{ width: '50px', height: '50px' }"></vue-loading>    
   </div>
 </template>
+
 <script>
 import { VueLoading } from 'vue-loading-template'
 export default {
